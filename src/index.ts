7c435b785e40d5c23434910bf254f57537a95c32
dsuplica <dsuplica@uchicago.edu>
2026-01-25 15:43:19 -0600

    update package.json and set up default exports

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { default as EyeLinkExtension } from "./extension-eyelink";
+export type { EyeLinkExtensionInterface } from "./extension-eyelink";
+export { default as EyeLinkPlugin } from "./plugin-eyelink-display";
